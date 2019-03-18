5beb075a71d96f41487f971fe26ae959315a8a5e
compgirl123 <claudia.f.feochari@hotmail.com>
2019-03-18 18:27:52 -0400

    #81 : Added basic Ui tests for login page

---
+++ test/Ui-Tests/MainActivity.java
@@ -0,0 +1,257 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package net.khertan.forrunners;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.apache.cordova.CordovaActivity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+
+public class MainActivity extends CordovaActivity
+{
+    public static final String KEY_URL_TO_LOAD = "KEY_URL_TO_LOAD";
+
+    @VisibleForTesting
+    //protected static final String WEB_FORM_URL = "file:///android_asset/web_form.html";
+    protected static final String WEB_FORM_URL = "http://10.0.2.2:8100/#/app/login";
+    //"file:///android_asset/www/templates/signin.html";
+    //"javascript:document.getElementById('usr').value = 'test@sf.com’;";
+    //"file:///android_asset/www/templates/signin.html";
+
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.loadUrl(urlFromIntent(getIntent()));
+        mWebView.requestFocus();
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+    }
+
+    private static String urlFromIntent(@NonNull Intent intent) {
+        checkNotNull(intent, "Intent cannot be null!");
+        String url = intent.getStringExtra(KEY_URL_TO_LOAD);
+        return !TextUtils.isEmpty(url) ? url : WEB_FORM_URL;
+    }
+    /*public static final String KEY_URL_TO_LOAD = "KEY_URL_TO_LOAD";
+    //protected static final String WEB_FORM_URL = "file:///android_asset/www/templates/signin.html";
+    @VisibleForTesting
+    protected static final String WEB_FORM_URL = "file:///android_asset/web_form.html";
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(urlFromIntent(getIntent()));
+        mWebView.requestFocus();
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+    }
+    private static String urlFromIntent(@NonNull Intent intent) {
+        checkNotNull(intent, "Intent cannot be null!");
+        String url = intent.getStringExtra(KEY_URL_TO_LOAD);
+        return !TextUtils.isEmpty(url) ? url : WEB_FORM_URL;
+    }*/
+}
+
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+/*package net.khertan.forrunners;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+// ADDITION HERE
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.apache.cordova.CordovaActivity;
+
+
+// ADDITION HERE
+
+public class MainActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+    }
+}
+*/
+// Thing below is what you uncomment to test
+/*
+
+
+package net.khertan.forrunners;
+
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.text.TextUtils;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+
+        import org.apache.cordova.CordovaActivity;
+
+        import androidx.annotation.NonNull;
+        import androidx.annotation.VisibleForTesting;
+
+        import static com.google.common.base.Preconditions.checkNotNull;
+
+
+public class MainActivity extends CordovaActivity
+{
+    public static final String KEY_URL_TO_LOAD = "KEY_URL_TO_LOAD";
+
+    @VisibleForTesting
+    //protected static final String WEB_FORM_URL = "file:///android_asset/web_form.html";
+    protected static final String WEB_FORM_URL = "http://10.0.2.2:8100/#/app/signin";
+    //"file:///android_asset/www/templates/signin.html";
+    //"javascript:document.getElementById('usr').value = 'test@sf.com’;";
+    //"file:///android_asset/www/templates/signin.html";
+
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.loadUrl(urlFromIntent(getIntent()));
+        mWebView.requestFocus();
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+    }
+
+    private static String urlFromIntent(@NonNull Intent intent) {
+        checkNotNull(intent, "Intent cannot be null!");
+        String url = intent.getStringExtra(KEY_URL_TO_LOAD);
+        return !TextUtils.isEmpty(url) ? url : WEB_FORM_URL;
+    }
+    /*public static final String KEY_URL_TO_LOAD = "KEY_URL_TO_LOAD";
+    //protected static final String WEB_FORM_URL = "file:///android_asset/www/templates/signin.html";
+    @VisibleForTesting
+    protected static final String WEB_FORM_URL = "file:///android_asset/web_form.html";
+    private WebView mWebView;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(urlFromIntent(getIntent()));
+        mWebView.requestFocus();
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+    }
+    private static String urlFromIntent(@NonNull Intent intent) {
+        checkNotNull(intent, "Intent cannot be null!");
+        String url = intent.getStringExtra(KEY_URL_TO_LOAD);
+        return !TextUtils.isEmpty(url) ? url : WEB_FORM_URL;
+    }
+}*/
