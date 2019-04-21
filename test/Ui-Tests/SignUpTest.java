39f8098d56a34519b434a7b60ed6dcd44131a9ca
compgirl123 <claudia.f.feochari@hotmail.com>
2019-04-20 21:35:20 -0400

    #81: Adding Passing Ui Test for Sign Up Page

--- test/Ui-Tests/SignUpTest.java
+++ test/Ui-Tests/SignUpTest.java
@@ -1,45 +1,26 @@
-
 package net.khertan.forrunners;
 
-import android.content.Intent;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.webkit.WebView;
-
-import org.hamcrest.Matchers;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.Array;
-
-import androidx.test.espresso.base.DefaultFailureHandler;
-import androidx.test.espresso.web.model.Atoms;
 import androidx.test.espresso.web.sugar.Web;
 import androidx.test.espresso.web.webdriver.DriverAtoms;
 import androidx.test.espresso.web.webdriver.Locator;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
-
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isJavascriptEnabled;
-import static androidx.test.espresso.web.assertion.WebViewAssertions.webContent;
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
-import static androidx.test.espresso.web.matcher.DomMatchers.hasElementWithId;
-import static androidx.test.espresso.web.model.Atoms.getCurrentUrl;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.clearElement;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
 
-
 import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
-import static org.hamcrest.CoreMatchers.equalTo;
-
 import static org.hamcrest.core.StringContains.containsString;
+
 /**
  * Basic sample that shows the usage of Espresso web showcasing {@link Web#onWebView()} API.
  * <p>
@@ -53,17 +34,66 @@ public class SignUpTest {
     @Rule
     public ActivityTestRule<MainActivity> mainactivitytest1 =new ActivityTestRule<MainActivity>(MainActivity.class);
 
-
-    private static final String goodEmailTest = "claudia.f.feochari@hotmail.com";
-    private static final String goodPasswordTest = "test12345";
-
-    private static final String badEmailTest = "testsamplewrongformattedemail";
-    private static final String badPasswordTest = "blablabla";
-
-    private static final String profileXpathemail = "/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/div[1]/span/button";
     private static final String menuXpath1 = "/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/div[1]/span/button";
-    private static final String signUpXpath = "/html/body/ion-nav-view/ion-side-menus/ion-side-menu/ion-content/div/ion-list/div/ion-item[3]/a";
+    private static final String signUpXpath = "/html/body/ion-nav-view/ion-side-menus/ion-side-menu/ion-content/div/ion-list/div/ion-item[2]/a";
 
+    @Test
+    public void signUpTestCheckInputsGood() throws Exception {
+
+        /**
+         * UI Sign Up Page Validation Test for Our ForRunners Application.
+         * This UI test checks if the username, email, password and confirm password
+         * text boxes are empty and are loaded and present on the page.
+         * It also checks if the Sign In button is there and has the appropriate label.
+         * Also, there are tests verifying if the labels on top of the text boxes are present and
+         * written appropriately.
+         */
+
+        Thread.sleep(7000);
+
+        onWebView()
+                .withElement(findElement(Locator.XPATH,menuXpath1)).perform(webClick())
+                .withElement(findElement(Locator.XPATH,signUpXpath)).perform(webClick());
+
+        Thread.sleep(2000);
+
+        onWebView().withElement(findElement(Locator.ID,"Username")).perform(DriverAtoms.webKeys(" "))
+                .check(webMatches(getText(),containsString("")));
+
+        onWebView().withElement(findElement(Locator.ID,"registeremail")).perform(DriverAtoms.webKeys(" "))
+                .perform(clearElement())
+                .check(webMatches(getText(),containsString("")));
+
+        onWebView().withElement(findElement(Locator.ID,"registerpassword")).perform(DriverAtoms.webKeys(" "))
+                .perform(clearElement())
+                .check(webMatches(getText(),containsString("")));
+
+        onWebView().withElement(findElement(Locator.ID,"registerconfirmPassword")).perform(DriverAtoms.webKeys(" "))
+                .perform(clearElement())
+                .check(webMatches(getText(),containsString("")));
+
+       onWebView().withElement(findElement(Locator.ID,"submitbutton"))
+               .check(webMatches(getText(),containsString("SIGN UP")));
+
+    }
+
+    @Test
+    public void signUpTestCheckInputsBad() throws Exception {
+
+        /**
+         * UI Sign Up Page Validation for Our ForRunners Application.
+         * It checks if the
+         */
+
+        Thread.sleep(7000);
+
+        onWebView()
+                .withElement(findElement(Locator.XPATH,menuXpath1)).perform(webClick())
+                .withElement(findElement(Locator.XPATH,signUpXpath)).perform(webClick())
+                .withElement(findElement(Locator.ID,"Username")).perform(DriverAtoms.webKeys(" "));
+        Thread.sleep(2000);
+
+    }
 
     @Test
     public void signUpPageTestAllBlanks() throws Exception{
